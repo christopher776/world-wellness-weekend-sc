6502923bd6ee72fd36db9cdd7f53a60d64f87eb3
christopher776 <christopher@luxwellsolutions.com>
2026-07-30 20:52:00 -0400

    Add instructor application to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,6 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/sponsorship",
     "/rentals",
     "/hotel-travel",
+    "/teach-a-class",
     "/association",
     "/rsvp",
     "/privacy-policy",
